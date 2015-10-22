0e18aca723d51fb4488ff366c5118bcbb7d29d26
MA Laforge <ma.laforge@example.com>
2015-10-21 20:40:47 -0400

    Make demo1 compare result of save/load operation

--- sample/demo1.jl
+++ sample/demo1.jl
@@ -3,6 +3,7 @@
 
 using EasyPlotMPL
 using EasyPlot
+using FileIO2
 
 #==Constants
 ===============================================================================#
@@ -10,7 +11,13 @@ using EasyPlot
 
 #==Render sample EasyPlot plots
 ===============================================================================#
+file(i::Int) = File{EPH5Fmt}("./sampleplotfile1.hdf5")
+
 plot = evalfile(EasyPlot.sampleplotfile(1));
 	display(Backend{:MPL}, plot)
+	save(plot, file(1))
+plot2=load(file(1))[1]; #Returns array of plots
+	plot2.title = "Compare Loaded File"
+	display(Backend{:MPL}, plot2)
 
 #Last line
