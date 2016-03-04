c44b1547124b2309a6e6f71715373c5d3a073e3d
MA Laforge <ma.laforge@example.com>
2016-03-04 12:21:55 -0500

    Fully support EasyPlot color system.
    Also remove some deprecated _line/_glyph code.

--- src/base.jl
+++ src/base.jl
@@ -4,44 +4,82 @@
 
 #==Constants
 ===============================================================================#
-const NCOLORS_GRACE = 15 #Number of colors expected to be defined by Grace.
+const NCOLORS_GRACEDFLT = 15 #Number of colors expected to be defined by Grace.
 #NOTE: Assuming color 0 is white/background color (not to be used)
+
+immutable FlagType{T}; end
+const NOTFOUND = FlagType{:NOTFOUND}()
 
 
 #==Base types
 ===============================================================================#
 typealias NullOr{T} Union{Void, T} #Simpler than Nullable
 
+#Manages additional colors (leaves default ones intact):
+#NOTE: 0 appears to be bkgnd color & 1: default frame color
+type ColorMgr
+	plt::GracePlot.Plot #Needed to send commands
+	colormap::Dict{ASCIIString, Int} #Maps #RRGGBB hex color string to registered color number
+	nextcolor::Int
+end
+ColorMgr(p::GracePlot.Plot) = ColorMgr(p, Dict(), NCOLORS_GRACEDFLT+1)
+
 type Axes{T} <: EasyPlot.AbstractAxes{T}
 	ref::GracePlot.GraphRef #Axes reference
 	theme::EasyPlot.Theme
+	colormgr::ColorMgr
 	eye::NullOr{EasyPlot.EyeAttributes}
 end
-Axes(style::Symbol, ref::GracePlot.GraphRef, theme::EasyPlot.Theme, eye=nothing) =
-	Axes{style}(ref, theme, eye)
+Axes(style::Symbol, ref::GracePlot.GraphRef, theme::EasyPlot.Theme, colormgr::ColorMgr, eye=nothing) =
+	Axes{style}(ref, theme, colormgr, eye)
 
 
-#==Helper/mapping functions
+#==Registering new colors
 ===============================================================================#
-warned_nocolorant = false
-function warn_nocolorant()
-	global warned_nocolorant
-	if !warned_nocolorant
-		info("Colorant values not yet supported.")
-		warned_nocolorant = true
+const HEX_CODES = UInt8[
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+]
+function int2hexcolorstr(v::UInt)
+	result = Array(UInt8, 7)
+	result[1] = '#'
+	for i in length(result):-1:2
+		result[i] = HEX_CODES[(v & 0xF)+1]
+		v >>= 4
 	end
+	return bytestring(result)
 end
 
-#no default... use auto-color
-#mapcolor(::Void) = mapcolor("black") #default
-mapcolor(v) = v #::Void uses built-in auto color
-mapcolor(v::Integer) = mod(v-1, NCOLORS_GRACE)+1 #use built-in Grace color map
-mapcolor(v::Symbol) = string(v) #Use built-in Grace color names
-function mapcolor(v::Colorant)
-	warn_nocolorant()
-	return nothing #Use built-in auto-color
+function buildcmd_mapcolor(id::Int, colorval::UInt, idstr::ASCIIString)
+	colorval = Int32(colorval) #Don't display results in hex
+	r = (colorval>>16) & 0xFF
+	g = (colorval>>8) & 0xFF
+	b = colorval & 0xFF
+	result = "map color $id to ($r,$g,$b), \"$idstr\""
 end
-mapfacecolor(v) = mapcolor(v) #In case we want to diverge
+
+#Automatically adds color if does not exist:
+function _getcoloridx(mgr::ColorMgr, v::Colorant)
+	v = convert(RGB24, v)
+	v = UInt(v.color)
+	colorid = int2hexcolorstr(v)
+	idx = get(mgr.colormap, colorid, NOTFOUND)
+	if idx != NOTFOUND
+		return idx
+	end
+
+	idx = mgr.nextcolor
+	cmd = buildcmd_mapcolor(idx, v, colorid)
+	GracePlot.sendcmd(mgr.plt, cmd)
+	mgr.colormap[colorid] = idx
+	mgr.nextcolor += 1
+	return idx
+end
+
+
+#==Mapping functions
+===============================================================================#
+mapcolor(mgr::ColorMgr, v::Colorant) = _getcoloridx(mgr, v)
+mapfacecolor(mgr::ColorMgr, v) = mapcolor(mgr, v) #In case we want to diverge
 
 #Linewidth:
 maplinewidth(w) = w
@@ -51,44 +89,25 @@ maplinewidth(::Void) = maplinewidth(1) #default
 mapglyphsize(sz) = sz/2
 mapglyphsize(::Void) = mapglyphsize(1) #default
 
-
-function _graceline(wfrm::EasyPlot.Waveform)
-	return line(style=wfrm.line.style,
-	            width=maplinewidth(wfrm.line.width),
-	            color=mapcolor(wfrm.line.color),
-	           )
-end
-
-function _graceglyph(wfrm::EasyPlot.Waveform)
-	color = wfrm.line.color
-	if nothing == color; color = wfrm.glyph.color; end
-	return glyph(shape=wfrm.glyph.shape,
-	             size=mapglyphsize(wfrm.glyph.size),
-	             linewidth=maplinewidth(wfrm.line.width),
-	             color=mapcolor(color),
-	             fillcolor=mapfacecolor(wfrm.glyph.color),
-	             fillpattern=(nothing==wfrm.glyph.color?0:1)
-	            )
-end
-
 #TODO: Support ColorScheme:
-function _graceline(attr::EasyPlot.WfrmAttributes)
+function _graceline(attr::EasyPlot.WfrmAttributes, mgr::ColorMgr)
 	return line(style=attr.linestyle,
 	            width=maplinewidth(attr.linewidth),
-	            color=mapcolor(attr.linecolor),
+	            color=mapcolor(mgr, attr.linecolor),
 	           )
 end
-function _graceglyph(attr::EasyPlot.WfrmAttributes)
-	nofill = (:transparent == attr.glyphfillcolor)
-	glyphfillcolor = nofill? (:white): attr.glyphfillcolor
+function _graceglyph(attr::EasyPlot.WfrmAttributes, mgr::ColorMgr)
+	nofill = (EasyPlot.COLOR_TRANSPARENT == attr.glyphfillcolor)
+	glyphfillcolor = nofill? EasyPlot.COLOR_WHITE: attr.glyphfillcolor
 	return glyph(shape=attr.glyphshape,
 	             size=mapglyphsize(attr.glyphsize),
 	             linewidth=maplinewidth(attr.linewidth),
-	             color=mapcolor(attr.glyphlinecolor),
-	             fillcolor=mapfacecolor(glyphfillcolor),
+	             color=mapcolor(mgr, attr.glyphlinecolor),
+	             fillcolor=mapfacecolor(mgr, glyphfillcolor),
 	             fillpattern=(nofill?0:1)
 	            )
 end
+
 
 #==Rendering functions
 ===============================================================================#
@@ -96,14 +115,15 @@ end
 #Called by EasyPlot, for each individual DataF1 ∈ DataMD.
 function EasyPlot.addwfrm(ax::Axes, d::DataF1, id::AbstractString,
 	la::EasyPlot.LineAttributes, ga::EasyPlot.GlyphAttributes)
-	attr = EasyPlot.WfrmAttributes(ax.theme, la, ga, resolvecolors=false) #Apply theme to attributes
-	_line = _graceline(attr)
-	_glyph = _graceglyph(attr)
+	attr = EasyPlot.WfrmAttributes(ax.theme, la, ga) #Apply theme to attributes
+	_line = _graceline(attr, ax.colormgr)
+	_glyph = _graceglyph(attr, ax.colormgr)
 	add(ax.ref, d.x, d.y, _line, _glyph, id=id)
 end
 
 #Render a paraticular subplot:
-function _render(g::GracePlot.GraphRef, subplot::EasyPlot.Subplot, theme::EasyPlot.Theme, displaylegend::Bool)
+function _render(g::GracePlot.GraphRef, subplot::EasyPlot.Subplot,
+	theme::EasyPlot.Theme, colormgr::ColorMgr, displaylegend::Bool)
 	set(g, subtitle = subplot.title)
 
 	#TODO Ugly: setting defaults like this should be done in EasyPlot
@@ -113,7 +133,7 @@ function _render(g::GracePlot.GraphRef, subplot::EasyPlot.Subplot, theme::EasyPl
 		if nothing == ep.teye; ep.teye = ep.tbit; end
 	end
 
-	axes = Axes(subplot.style, g, theme, ep)
+	axes = Axes(subplot.style, g, theme, colormgr, ep)
 
 	for (i, wfrm) in enumerate(subplot.wfrmlist)
 		EasyPlot.addwfrm(axes, wfrm, i)
@@ -141,6 +161,7 @@ end
 
 function EasyPlot.render(gplot::GracePlot.Plot, eplot::EasyPlot.Plot; ncols::Int=1)
 	nrows = div(length(eplot.subplots)-1, ncols)+1
+	colormgr = ColorMgr(gplot)
 
 	#Arrange basically allocates all subplots (GracePlot.Plot):
 	arrange(gplot, (nrows, ncols))
@@ -148,7 +169,7 @@ function EasyPlot.render(gplot::GracePlot.Plot, eplot::EasyPlot.Plot; ncols::Int
 
 	for s in eplot.subplots
 		g = graph(gplot, graphidx)
-		_render(g, s, eplot.theme, eplot.displaylegend)
+		_render(g, s, eplot.theme, colormgr, eplot.displaylegend)
 		graphidx += 1
 	end
 
