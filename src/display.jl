84c89b17d24a47377e092457b641efd4fecc57f6
MA Laforge <ma.laforge@example.com>
2016-03-22 22:25:14 -0400

    Control defaults.renderdpi from ENV.
    Render inline plots @ 75dpi.

--- src/display.jl
+++ src/display.jl
@@ -2,15 +2,45 @@
 #-------------------------------------------------------------------------------
 
 
+#==Constants
+===============================================================================#
+DEFAULT_RENDERDPI = 75 #Low res for inline graphics
+
+
+#==Defaults
+===============================================================================#
+type Defaults
+	renderdpi::Int #Low res for inline graphics
+end
+
+function Defaults()
+	const ENVSTR_RENDERDPI = "EASYPLOTGRACE_RENDERDPI"
+	renderdpi = get(ENV, ENVSTR_RENDERDPI, "$DEFAULT_RENDERDPI")
+
+	try
+		renderdpi = parse(Int, renderdpi)
+	catch
+		warn("Invalid value for $ENVSTR_RENDERDPI: $renderdpi.  Setting to $(DEFAULT_RENDERDPI).")
+		renderdpi = DEFAULT_RENDERDPI
+	end
+
+	Defaults(renderdpi)
+end
+
+const defaults = Defaults()
+
+
 #==Types
 ===============================================================================#
 #TODO: support guimode=false
 
 type PlotDisplay <: EasyPlot.EasyPlotDisplay #Don't export.  Qualify with Module
 	guimode::Bool
+	dpi::Int
 	args::Tuple
 	kwargs::Vector{Any}
-	PlotDisplay(args...; guimode=true, kwargs...) = new(guimode, args, kwargs)
+	PlotDisplay(args...; guimode=true, dpi=GracePlot.DEFAULT_DPI, kwargs...) =
+		new(guimode, dpi, args, kwargs)
 end
 
 
@@ -23,7 +53,8 @@ function EasyPlot._display(plot::GracePlot.Plot)
 end
 
 function EasyPlot.render(d::PlotDisplay, eplot::EasyPlot.Plot)
-	return render(GracePlot.new(d.args...; guimode=d.guimode, d.kwargs...), eplot)
+	plot = GracePlot.new(d.args...; guimode=d.guimode, dpi=d.dpi, d.kwargs...)
+	return render(plot, eplot)
 end
 
 Base.mimewritable(mime::MIME, eplot::EasyPlot.Plot, d::PlotDisplay) =
@@ -34,7 +65,7 @@ Base.mimewritable(mime::MIME, eplot::EasyPlot.Plot, d::PlotDisplay) =
 ===============================================================================#
 EasyPlot.registerdefaults(:Grace,
 	maindisplay = PlotDisplay(guimode=true),
-	renderdisplay = PlotDisplay(guimode=false)
+	renderdisplay = PlotDisplay(guimode=false, dpi=defaults.renderdpi)
 )
 
 #Last line
