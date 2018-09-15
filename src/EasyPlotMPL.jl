20c8c68c7b3cf47729838dfd36931e63c6e4ffb9
MA Laforge <ma.laforge.49@gmail.com>
2018-09-15 16:28:05 -0700

    More improvements to module initialization (+hack).

--- src/EasyPlotMPL.jl
+++ src/EasyPlotMPL.jl
@@ -29,6 +29,9 @@ function __init__()
 		maindisplay = PlotDisplay(guimode=true),
 		renderdisplay = PlotDisplay(guimode=false)
 	)
+	show(:HACK_SHOWTOUNFREEZE) #Not sure why show does this
+	println(" - EasyPlotMPL")
+	return
 end
 
 end
