80120f1ce63f10984f727759452e8340de1b0c87
MA Laforge <ma.laforge.49@gmail.com>
2020-07-14 15:38:51 -0400

    Remove deprecated files, and move a few more.

--- subpkgs/EasyPlotGrace/test/runtests.jl
+++
@@ -1,7 +0,0 @@
-#Test code
-#-------------------------------------------------------------------------------
-
-#No real test code yet... just run demos:
-include("../sample/runsamples.jl")
-
-:Test_Complete
