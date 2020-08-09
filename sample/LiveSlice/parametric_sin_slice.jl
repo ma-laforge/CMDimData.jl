39a2894a38c169436ccdb290a6c4437716c0ca60
MA Laforge <ma.laforge.49@gmail.com>
2020-08-08 21:49:25 -0400

    Get ready to add new example.

---
+++ sample/LiveSlice/parametric_sin_slice.jl
@@ -0,0 +1,92 @@
+#"Live-Slice" the results of a parametric sinusoidal "simulation
+#-------------------------------------------------------------------------------
+module CMDimData_SampleDemo
+
+using CMDimData
+using CMDimData.MDDatasets
+using CMDimData.EasyPlot
+CMDimData.@includepkg EasyPlotInspect
+pdisp = EasyPlotInspect.PlotDisplay()
+
+
+#==Constants
+===============================================================================#
+LBL_AXIS_TIME = "Time (s)"
+LBL_AXIS_POSITION = "Position (m)"
+LBL_AXIS_NORMPOSITION = "Normalized Pos (m/m)"
+LBL_AXIS_SPEED = "Speed (m/s)"
+lstylesweep = cons(:a, line = set(style=:solid, width=2)) #Default is a bit thin
+dfltglyph = cons(:a, glyph = set(shape=:o, size=1.5))
+
+
+#==Read in results of parametric "simulation"
+===============================================================================#
+signal = evalfile("../parametric_sin_data.jl")
+
+
+#==Parameter extraction
+===============================================================================#
+
+#Generate DataRS with value of amplitude (A) in each parameter combination:
+ampvalue = parameter(signal, "A")
+
+#Normalize signal amplitudes for ALL signals SIMULTANEOUSLY:
+signal_norm = signal / ampvalue
+
+#Compute continuous-time signal rate for ALL signals SIMULTANEOUSLY:
+#   signal(𝜙, A, 𝑓, t) -> rate(𝜙, A, 𝑓):
+rate = deriv(signal)
+
+#Compute maximum signal rate for ALL signals SIMULTANEOUSLY:
+maxrate = maximum(abs(rate))
+
+#Compute first fall-crossing point for ALL signals SIMULTANEOUSLY:
+#   signal(𝜙, A, 𝑓, t) -> fallx(𝜙, A, 𝑓):
+fallx = xcross1(signal, xstart=0, allow=CrossType(:fall))
+
+
+#==Generate plots
+===============================================================================#
+xred1 = paramlist(fallx)[end] #x-value of 1st reduction operation (finding 1st-xing time)
+yext_firstx = (min=90e-6, max=130e-6) #Common y-axis extents to compare times @ first crossing
+
+#plot #1: Complete time-domain signals
+#-------------------------------------------------------------------------------
+p1 = cons(:plot, nstrips = 3,
+	ystrip1 = set(axislabel=LBL_AXIS_POSITION, striplabel="Sinusoidal response"),
+	ystrip2 = set(axislabel=LBL_AXIS_NORMPOSITION, striplabel="Normalized response (All peaks should be ±1)"),
+	ystrip3 = set(axislabel=LBL_AXIS_SPEED, striplabel="Rate of change (should be larger for higher frequencies)"),
+	xaxis = set(label=LBL_AXIS_TIME)
+)
+push!(p1,
+	cons(:wfrm, signal, lstylesweep, label="", strip=1),
+	cons(:wfrm, signal_norm, lstylesweep, label="", strip=2),
+	cons(:wfrm, rate, lstylesweep, label="", strip=3),
+)
+
+#plot #2: Extracted parameter values: fallx, maxrate
+#-------------------------------------------------------------------------------
+p2 = cons(:plot, nstrips = 2,
+	ystrip1 = set(axislabel=LBL_AXIS_TIME, striplabel="Time to 1st fall-crossing"), #; yext_firstx...),
+	ystrip2 = set(axislabel=LBL_AXIS_SPEED, striplabel="Maximum signal rate"), #; yext_firstx...),
+	xaxis = set(label=xred1),
+)
+push!(p2,
+	cons(:wfrm, fallx, lstylesweep, dfltglyph, label="", strip=1),
+	cons(:wfrm, maxrate, lstylesweep, dfltglyph, label="", strip=2),
+)
+
+println()
+@info("Displaying plot...")
+pcoll = push!(cons(:plotcoll, title="Parametric sin() - Live-Slice Results"), p1, p2)
+	pcoll.ncolumns=2
+	pcoll.displaylegend=true
+display(pdisp, pcoll)
+
+#Activate Live-Slice GUI:
+println()
+@info("Activating Live-Slice environment...")
+#include("tools/liveslice.jl")
+#LiveSlice._slice(p2.wfrmlist, pcoll)
+
+end #module
