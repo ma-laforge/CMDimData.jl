d3bafbe30ec76880bb41f296c821578a88d913f3
MA Laforge <ma.laforge.49@gmail.com>
2020-08-15 22:18:34 -0400

    Update EasyPlotPlots sample file.

--- sample/EasyPlotPlots/runsamples.jl
+++ sample/EasyPlotPlots/runsamples.jl
@@ -1,29 +1,49 @@
-#EasyPlotPlots demonstration 1: Render sample EasyPlot plots
+#Run sample code
 #-------------------------------------------------------------------------------
 
-using EasyPlot
-using EasyPlotPlots
-using FileIO2
+module CMDimData_SampleGenerator
+
+using CMDimData
+using CMDimData.EasyPlot
+CMDimData.@includepkg EasyPlotPlots
 
 
 #==Constants
 ===============================================================================#
-#renderingtool = :pyplot #Python based
-renderingtool = :inspectdr
-#renderingtool = :gadfly #Deprecated
-#renderingtool = :gr
-#renderingtool = :pgfplots
-#renderingtool = :plotly #Browser based?
-#renderingtool = :glvisualize #3D-heavy
-#renderingtool = :unicodeplots #Text-based
+demolist = EasyPlot.demofilelist()
+renderingtool = :gr
+	#no display: {:hdf5}
+	#Text-based {:unicodeplots}
+	#GUI: {:gr, :inspectdr, :pgfplotsx}
+	#Python-based {:pyplot}
+	#Browser-based? {:plotly, :plotlyjs}
+	##########deprecated: {:gadfly, :bokeh, :winston}
 #pdisp = EasyPlotPlots.PlotDisplay()
 pdisp = EasyPlotPlots.PlotDisplay(renderingtool)
 
 
+#==Helper functions
+===============================================================================#
+printsep(title) = println("\n", title, "\n", repeat("-", 80))
+
+
 #==Render sample EasyPlot plots
 ===============================================================================#
-plot = evalfile(EasyPlot.sampleplotfile(1));
+plot = evalfile(demolist[1])
+#	display(pdisp, plot)
+	EasyPlot.write_png("image.png", plot, pdisp)
+#	EasyPlot.write_svg("image.svg", plot, pdisp)
+
+
+#==Render sample EasyPlot plots
+===============================================================================#
+for demofile in demolist[1:end]
+	fileshort = basename(demofile)
+	printsep("Executing $fileshort...")
+	plot = evalfile(demofile)
 	display(pdisp, plot)
-	EasyPlot._write(File(:png, "image.png"), plot, pdisp)
-#	EasyPlot._write(File(:svg, "image.svg"), plot, pdisp)
+end
+
+end
+:SampleCode_Executed
 #Last line
